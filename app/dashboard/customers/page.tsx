28b5b7c6f67f5dccc3ba0c21140ad77acfed93f1
dvinubius <dvinubiu.narmon@gmail.com>
2023-11-02 18:30:54 +0200

    customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,22 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { lusitana } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+import CustomersTable from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import { CustomerField, FormattedCustomersTable } from '@/app/lib/definitions';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const customers = await fetchFilteredCustomers(searchParams?.query || '');
+  
+  return <CustomersTable customers={customers} />;
 }
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,10 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
- 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+}; 
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
